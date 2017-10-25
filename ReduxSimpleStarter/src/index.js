06ffe96c57de33bf081a582b27a9f7036ec72556
Tomi Salin <tomi.salin@hotmail.com>
2017-10-25 14:31:02 +0300

    Wire everything up, make search bar work, show video, make video list clickable and add some styles

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-const API_KEY = 'YOUR API KEY';
+import VideoDetail from './components/video_detail'
+const API_KEY = 'YOUR_API_KEY';
 
 class App extends Component {
 constructor(props) {
   super(props);
 
-  this.state = { videos: [] };
+  this.state = {
+    videos: [],
+    selectedVideo: null
+   };
 
-  YTSearch({key:API_KEY, term:'world of warcraft'}, (videos) => {
-    this.setState({ videos });
-    console.log(videos);
-  });
+   this.videoSearch('surfboards')
 }
 
+
+  videoSearch(term) {
+    YTSearch({key:API_KEY, term:term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+
+
   render() {
+    const videoSearch= _.debounce((term) => { this.videoSearch(term) }, 300);
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar
+          onSearchTermChange={videoSearch}
+        />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
