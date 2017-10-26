b592d1ff386280e18f915e8bde6438ac562d5710
Tomi Salin <tomi.salin@hotmail.com>
2017-10-26 15:54:53 +0300

    Add BookList and make it container, add BookDetail action

---
+++ ReduxExercise/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: the good parts'},
+    {title: 'Fellowship of the ring'},
+    {title: 'Java is interresting'},
+    {title: 'Art of War'}
+  ];
+}
