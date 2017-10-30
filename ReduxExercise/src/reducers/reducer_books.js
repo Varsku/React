17f2c57fed99e0780678a9d14fb474c1a58b658f
Tomi Salin <tomi.salin@hotmail.com>
2017-10-30 09:53:42 +0200

    Add book detail showing on click in ReduxExercise and fix cityweather

--- ReduxExercise/src/reducers/reducer_books.js
+++ ReduxExercise/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Javascript: the good parts'},
-    {title: 'Fellowship of the ring'},
-    {title: 'Java is interresting'},
-    {title: 'Art of War'}
+    {title: 'Javascript: the good parts', pages: 101},
+    {title: 'Fellowship of the ring', pages: 300},
+    {title: 'Java is interresting', pages: 20},
+    {title: 'Art of War', pages: 300}
   ];
 }
