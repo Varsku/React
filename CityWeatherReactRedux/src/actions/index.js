653f506db879fd59693112bc3fd378f05909d98f
Tomi Salin <tomi.salin@hotmail.com>
2017-11-07 11:31:35 +0200

    Minor fix

--- CityWeatherReactRedux/src/actions/index.js
+++ CityWeatherReactRedux/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'f54b4be2f1411da907dc3a7e40a53432';
+const API_KEY = 'Your api key';
 const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER= 'FETCH_WEATHER';
