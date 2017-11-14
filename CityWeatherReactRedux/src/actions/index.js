f6f6be5791c3802be181f7ac3fef12ed50c7b59b
Tomi Salin <tomi.salin@hotmail.com>
2017-11-14 10:39:58 +0200

    Add chart for humidity and pressure, add CSS to make them look better

--- CityWeatherReactRedux/src/actions/index.js
+++ CityWeatherReactRedux/src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const API_KEY = 'Your api key';
+const API_KEY = 'YOUR API KEY';
 const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER= 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city},us&units=metric`;
   const request = axios.get(url);
 
 
